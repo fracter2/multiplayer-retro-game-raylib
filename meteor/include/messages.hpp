cb90a15d44257315fcceef31cf6fa5789a5ac1ed
Theodor <fracter.giftogod@gmail.com>
2025-10-29 18:45:31 +0100

    Create new game_state_message and input_action_message

--- meteor/include/messages.hpp
+++ meteor/include/messages.hpp
@@ -4,59 +4,39 @@
 
 #include "raylib.h"
 #include "network.hpp"
+#include "game.hpp"
 
 namespace meteor
 {
    enum class message_type : uint8 {
-      LATENCY,
-      POSITION,
-      ENTITY_STATE
+	  GAME_STATE,
+	  //GAME_DELTA,
+	  //TICK_WRAP,	// Used to state the tick for the following messages	// TODO CONSIDER REMOVING we can just add tick to message...
+	  INPUT_ACTION
    };
 
-   enum class movement_request : uint8 {
-       RIGHT,
-       LEFT,
-       UP,
-       DOWN,
-       NEUTRAL
+   struct game_state_message {
+	   //game_state_message() = default;
+	   game_state_message(game::game_state state);
+
+	   bool write(byte_stream_writer& writer);
+	   bool read(byte_stream_reader& reader);
+
+	   message_type		m_type = message_type::GAME_STATE;
+	   game::game_state m_game_state = {};
    };
 
-   // TODO input action message
+   struct input_action_message {
+	   //game_state_message() = default;
+	   input_action_message(game::player_actions action);
 
-   struct entity_state_message {
-      entity_state_message() = default;
-      entity_state_message(int32 id, Vector2 position, Color color, movement_request move_request);
+	   bool write(byte_stream_writer& writer);
+	   bool read(byte_stream_reader& reader);
 
-      bool write(byte_stream_writer& writer);
-      bool read(byte_stream_reader& reader);
-      
-      uint8   m_type = 0;
-      int32   m_id = 0;
-      Vector2 m_position = {};
-      Color   m_color = {};
-      uint8   m_move_request = 0;
+	   message_type			m_type = message_type::INPUT_ACTION;
+	   game::player_actions m_action = game::player_actions::INVALID;
    };
 
-   struct latency_message {
-      latency_message() = default;
-      latency_message(double time);
+   
 
-      bool write(byte_stream_writer& writer);
-      bool read(byte_stream_reader& reader);
-
-      uint8  m_type = 0;
-      double m_time = 0;
-   };
-
-   struct mouse_position_message {
-      mouse_position_message() = default;
-      mouse_position_message(float x, float y);
-
-      bool write(byte_stream_writer& writer);
-      bool read(byte_stream_reader& reader);
-
-      uint8 m_type = 0;
-      float m_x = 0;
-      float m_y = 0;
-   };
 } // !meteor
--- meteor/src/messages.cpp
+++ meteor/src/messages.cpp
@@ -4,92 +4,85 @@
 
 namespace meteor
 {
-   entity_state_message::entity_state_message(int32 id, Vector2 position, Color color, movement_request move_request)
-      : m_type((uint8)message_type::ENTITY_STATE)
-      , m_id(id)
-      , m_position(position)
-      , m_color(color)
-      , m_move_request((uint8)move_request)
-   {
-   }
+	// ---- game_state_message ----
+	
+
+	game_state_message::game_state_message(game::game_state state)
+		: m_type(message_type::GAME_STATE)
+		, m_game_state(state)
+	{
+	}
+
+	bool game_state_message::write(byte_stream_writer& writer) { return serialize(*this, writer); }
+	bool game_state_message::read(byte_stream_reader& reader) { return serialize(*this, reader); }
+
+	template <typename T>
+	bool serialize(game_state_message& message, T& stream)
+	{
+		bool success = true;
+		success &= stream.serialize(message.m_type);
+		success &= stream.serialize(message.m_game_state);
+		return success;
+	}
+
+	template <typename T>
+	bool serialize(game::game_state& state, T& stream) {
+		bool success = true;
+		for (int i = 0; i < game::MAX_PLAYERS; i++) {
+			success &= stream.serialize(state.m_bombs[i]);
+			success &= stream.serialize(state.m_players[i]);
+		}
+		success &= stream.serialize(state.m_tilemap);
+
+	}
+
+	template <typename T>
+	bool serialize(game::player_entity& player, T& stream) {
+		bool success = true;
+		success &= stream.serialize(player.m_dead);
+		success &= stream.serialize(player.m_position.x);
+		success &= stream.serialize(player.m_position.y);
+		success &= stream.serialize(player.m_velocity.x);
+		success &= stream.serialize(player.m_velocity.y);
+		return success;
+	}
+
+	template <typename T>
+	bool serialize(game::bomb& bomb, T& stream) {
+		bool success = true;
+		
+		success &= stream.serialize(bomb.m_explosion_tick);
+		success &= stream.serialize(bomb.m_position.x);
+		success &= stream.serialize(bomb.m_position.y);
+		return success;
+	}
+
+
+
+
+	// ---- input_action_message ----
+
+	bool input_action_message::write(byte_stream_writer& writer) { return serialize(*this, writer); }
+	bool input_action_message::read(byte_stream_reader& reader) { return serialize(*this, reader); }
+
+	input_action_message::input_action_message(game::player_actions action) 
+		: m_type(message_type::INPUT_ACTION)
+		, m_action(action)
+	{
+	}
 
    template <typename T>
-   bool serialize(entity_state_message &message, T& stream)
-   {
-      bool success = true;
-      success &= stream.serialize(message.m_type);
-      success &= stream.serialize(message.m_id);
-      success &= stream.serialize(message.m_position.x);
-      success &= stream.serialize(message.m_position.y);
-      success &= stream.serialize(message.m_color.r);
-      success &= stream.serialize(message.m_color.g);
-      success &= stream.serialize(message.m_color.b);
-      success &= stream.serialize(message.m_color.a);
-      return success;
+   bool serialize(input_action_message& message, T& stream) {
+	   bool success = true;
+	   success &= stream.serialize(message.m_type);
+	   success &= stream.serialize(player.m_position.x);
+	   success &= stream.serialize(player.m_position.y);
+	   success &= stream.serialize(player.m_velocity.x);
+	   success &= stream.serialize(player.m_velocity.y);
+	   return success;
    }
 
-   bool entity_state_message::write(byte_stream_writer& writer)
-   {
-      return serialize(*this, writer);
-   }
-
-   bool entity_state_message::read(byte_stream_reader& reader)
-   {
-      return serialize(*this, reader);
-   }
-
-   latency_message::latency_message(double time)
-      : m_type((uint8)message_type::LATENCY)
-      , m_time(time)
-   {
-   }
-
-   template <typename T>
-   bool serialize(latency_message &message, T& stream)
-   {
-      bool success = true;
-      success &= stream.serialize(message.m_type);
-      success &= stream.serialize(message.m_time);
-      return success;
-   }
-
-   bool latency_message::write(byte_stream_writer& writer)
-   {
-      return serialize(*this, writer);
-   }
-
-   bool latency_message::read(byte_stream_reader& reader)
-   {
-      return serialize(*this, reader);
-   }
-
-   mouse_position_message::mouse_position_message(float x, float y)
-      : m_type((uint8)message_type::POSITION)
-      , m_x(x)
-      , m_y(y)
-   {
-   }
-
-   template <typename T>
-   bool serialize(mouse_position_message &message, T& stream)
-   {
-      bool success = true;
-      success &= stream.serialize(message.m_type);
-      success &= stream.serialize(message.m_x);
-      success &= stream.serialize(message.m_y);
-      return success;
-   }
-
-   bool mouse_position_message::write(byte_stream_writer& writer)
-   {
-      return serialize(*this, writer);
-   }
-
-   bool mouse_position_message::read(byte_stream_reader& reader)
-   {
-      return serialize(*this, reader);
-   }
-
+   
    
 
 } // !meteor
