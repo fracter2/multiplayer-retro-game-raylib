b01cf9cc322a2ffbca5b9aa7bc58c3063da062f4
Theodor <fracter.giftogod@gmail.com>
2025-11-05 05:00:23 +0100

    Add m_last_recieve and similar to connection class to track acks better

--- meteor/include/connection.hpp
+++ meteor/include/connection.hpp
@@ -5,34 +5,47 @@
 #include "network.hpp"
 
 namespace meteor {
+	
+
 	struct connection {
 		enum class status {
-			DISCONNECTED,		// not connected, accept broadcasts
+			DISCONNECTED,		// not connected, accepts broadcasts
 			CONNECTING,			// attempting to connect to endpoint (sent connect pck, awaiting connect pck response with player index)
 			CONNECTED,			// connected
 			DISCONNECTING,		// attempting gracefull disconnect (sent disconnect pck awaiting response)
 			TEXAS,				// texas
 		};
 
+		struct stats {
+			static constexpr uint32 MAX_LOGGED_PACKETS = 20 * 10; // Max logged packets. 20/s sent packets * 10 sec
+			
+		};
+
 		connection() = default;
-		connection(uint32 id, ip_endpoint endpoint, double last_recieve_time)
-			: m_id(id)
-			, m_endpoint(endpoint)
-			, m_last_recieve_time(last_recieve_time)
-			, m_status(status::DISCONNECTED)
-			, m_sequence(0)
+		connection(ip_endpoint endpoint, status status = status::DISCONNECTED)
+			: m_endpoint(endpoint)
+			, m_last_recieve_time(0)
+			, m_status(status)
+			, m_send_sequence(0)
+			, m_recieve_sequence(0)
+			, m_recieve_acknowledge(0)
 		{
 		}
 
-		uint32		m_id			 = 0;
-		ip_endpoint m_endpoint;
-		status		m_status		 = {};
-		double		m_last_recieve_time = 0;
-		uint32		m_sequence		 = 0; // What we send
-		uint32		m_acknowledge	 = 0; // recieved sequence, used as ack
+		//uint32		m_id				  = 0;
+		ip_endpoint m_endpoint			  = {};
+		status		m_status			  = status::DISCONNECTED;
+		double		m_last_recieve_time   = 0;
+		uint32		m_send_sequence		  = 0;
+		uint32		m_recieve_sequence	  = 0;	// Used as send-ack
+		uint32		m_recieve_acknowledge = 0;
+
+		
 	};
 
-	// Client -> server system state
+	
+
+
 	/*
 	struct server_connection_syncer {
 		server_connection_syncer() = default;
