37ab01bc8a7f83079ef45db83b32d5690ff8c488
Theodor <fracter.giftogod@gmail.com>
2025-11-24 23:44:21 +0100

    Add funcs for setting skip-recieve flag on connections / server

--- meteor/src/ui.cpp
+++ meteor/src/ui.cpp
@@ -4,11 +4,23 @@
 
 #include "ui.hpp"
 
-
 namespace meteor::ui {
-	void quit_check(connection& conn, const input::input_state& input) {
+	void quit_check(connection& conn, const input_state& input) {
 		if (input.m_1_just_pressed && conn.get_status() == connection::status::CONNECTED) {
 			conn.set_disconnecting();
 		}
 	}
+
+	void debug_skip_recieve_check(connection& conn, const input_state& input) {
+		if (input.m_5) { conn.m_debug_skip_recieve = true; }
+		else { conn.m_debug_skip_recieve = false; }
+	}
+
+#ifdef _SERVER
+
+	void debug_skip_recieve_check(server_state& server, const input_state& input) {
+		if (input.m_5) { server.m_debug_skip_recieve = true; }
+		else { server.m_debug_skip_recieve = false; }
+	}
+#endif
 }
