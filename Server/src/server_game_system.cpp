4dba87e003b7b3e632cb60f4c28b8a2ed89de046
Theodor <fracter.giftogod@gmail.com>
2025-11-24 12:56:35 +0100

    Make all non-active player states set input delay to 0

--- Server/src/server_game_system.cpp
+++ Server/src/server_game_system.cpp
@@ -69,9 +69,10 @@ namespace meteor::server_game_system {
 			}
 			
 			// PARSE INPUT FOR SERVER to controll bot
-			else if (game_instance.m_player_info[player_index].m_player_status == player_info::status::ACTIVE_BOT) {
+			else if (game_instance.m_player_info[player_index].m_player_status != player_info::status::ACTIVE) {
 				player.m_prev_action_tick = game_instance.m_state.m_tick;
-				if (!bot_controlled) {
+
+				if (game_instance.m_player_info[player_index].m_player_status != player_info::status::ACTIVE_BOT && !bot_controlled) {
 					bot_controlled = true;
 					player.m_prev_action = input_to_player_action(input_state, game_instance.m_state, player_index);
 				}
